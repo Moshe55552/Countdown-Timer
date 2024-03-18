dc5a666db12998b49e489c84a0fb17fbcf7f3c3a
Moshe55552 <80269537+Moshe55552@users.noreply.github.com>
2024-03-18 09:53:44 +0200

    src

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CountdownTimer from "./Components/countdownTimer";
+
+function App() {
+  return (
+    <div>
+      <CountdownTimer />
+    </div>
+  );
+}
+
+export default App;
